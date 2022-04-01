1f9411f9ea641f30b03a5f1656c4d502f3f2baf1
Elias De Meyere <elias.de.meyere@student.ehb.be>
2022-04-01 18:41:15 +0200

    Oef4

---
+++ WC2/js/oef4.js
@@ -0,0 +1,41 @@
+'use strict';
+console.log("Loaded!")
+
+let student = {
+    name: "",
+    age: "",
+    degree: "",
+    courses: [""],
+
+    setPersonalDetails(name, age, degree){
+        this.name = name
+        this.age = age
+        this.degree = degree
+    },
+
+    addCourse(course){
+        this.courses.push(course);
+    },
+
+    showStudent(){
+        console.log("Student data: " + this.name + ", " + this.age + ", " + this.degree + "\nThey follow: " + this.courses)
+    }
+}
+
+let naam = prompt("Wat is uw naam?");
+let leeftijd = prompt("Hoe oud bent u?");
+let richting = prompt("Welke richting volgt u?");
+
+student.setPersonalDetails(naam, leeftijd, richting)
+
+while (true){
+    let c = prompt('Course?');
+    if (c){
+        student.addCourse(c);
+    }
+    else {
+        break;
+    }
+}
+
+student.showStudent();
