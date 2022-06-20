d406db825cca4e640dbe2ebd76c8aef117293f11
Elias De Meyere <elias.de.meyere@student.ehb.be>
2022-06-20 12:13:21 +0200

    animaties

--- WC4/DeelA/script2.js
+++ WC4/DeelA/script2.js
@@ -6,43 +6,22 @@ console.log(getal)
 
 window.onload = () => {
     console.log("LOADED");
-    
-    document.getElementById("form").addEventListener('submit', function(evt){
-        evt.preventDefault();
-        let gok = document.getElementById("guess").value;
-        console.log(gok)
-        return gok;
-    });
-
-
-    const raadGetal = new Promise(function(WelGetal, GeenGetal) {
-        if (isNaN(guess)){
-            return GeenGetal
-        } else if (guess > getal) {
-            return WelGetal('A');
-        } else if (guess < getal) {
-            return WelGetal('B');
-        } else {
-            return WelGetal('C')
-        }
-    });
-
-    raadGetal.then(
-        function GeenGetal(){
-            alert("Error! Gelieve een getal in te voeren")
-        },
-        function WelGetal(value){
-            switch (value){
-                case 'A': 
-                    console.log("Te hoog")
-                    break;
-                case 'B':
-                    console.log("Te laag")
-                    break;
-                case 'C':
-                    console.log('U heeft het getal geraden!');
-                    break;
+     
+    let raadGetal = new Promise((WelGetal, GeenGetal) => {
+        document.getElementById("form").addEventListener('submit', function(evt){
+            evt.preventDefault();
+            let gok = document.getElementById("gok").value;
+            if (isNaN(gok) || gok == null){
+                GeenGetal("Error, not a number!")
+            }else {
+                return WelGetal('Well een nummer kennelijk')
             }
-        }
-    );
+        });   
+    });
+
+    raadGetal.then((message) => {
+        console.log("This is the then, " + message);
+    }).catch((message) => {
+        console.log(message);
+    });
 }
